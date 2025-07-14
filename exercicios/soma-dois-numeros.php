a47de7363a592cb74b1ddfbd1fe1921ac16ef94d
Matheus Neris <matheus.nerisxa@outlook.com>
2025-07-14 20:56:25 -0300

    Soma de dois números

---
+++ exercicios/soma-dois-numeros.php
@@ -0,0 +1,10 @@
+<?php
+//Crie uma função chamada soma que receba dois números como parâmetros
+// e retorne a soma deles.
+
+function soma($numero1, $numero2)
+{
+    echo $numero1 + $numero2;
+}
+
+soma(5,8);
