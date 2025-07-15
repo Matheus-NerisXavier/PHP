f6d4f561ab11b29e519c62d3f39d1f868d79686c
Matheus Neris <matheus.nerisxa@outlook.com>
2025-07-15 20:57:08 -0300

    Strings numéricas

---
+++ manipulando-textos/numeros.php
@@ -0,0 +1,13 @@
+<?php
+
+$anoNascimento = '2001';
+
+$idade = 2025 - $anoNascimento;
+
+echo $idade .PHP_EOL;
+
+if ($anoNascimento == 2001) {
+    echo "SIM" . PHP_EOL;
+} else {
+    echo "NÃO" . PHP_EOL;
+}
