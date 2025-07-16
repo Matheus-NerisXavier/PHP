f29cd1563d5399c002621e42f9c592545e8f167a
Matheus Neris <matheus.nerisxa@outlook.com>
2025-07-16 20:21:01 -0300

    ordenando arrays

---
+++ manipulando-arrays/notas.php
@@ -0,0 +1,14 @@
+<?php
+
+$notas = [
+    "Matheus" => 10,
+    "Lucas" => 8,
+    "Ana" => 9,
+    "Luana" => 7,
+    "Paula" => 6
+];
+
+//arsort($notas);
+//uksort($notas);
+krsort($notas);
+var_dump($notas);
