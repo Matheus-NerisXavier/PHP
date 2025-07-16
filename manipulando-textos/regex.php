c725136690aa62df058a624d5446f82e75cf6f9f
Matheus Neris <matheus.nerisxa@outlook.com>
2025-07-16 18:45:21 -0300

    substituições com regex

--- manipulando-textos/regex.php
+++ manipulando-textos/regex.php
@@ -3,11 +3,18 @@
 $telefones = ['(19) 98765 - 5332', '(19) 97265 - 1232', '(19) 95765 - 5222'];
 
 foreach ($telefones as $telefone) {
-
-    $telefoneValido = preg_match('/^\([0-9]{2}\) 9?[0-9]{4} - [0-9]{4}$/', $telefone);
+    $regex = '/^\(([0-9]{2})\) (9?[0-9]{4} - [0-9]{4})$/';
+    $telefoneValido = preg_match($regex, $telefone);
     if($telefoneValido) {
         echo 'Telefone valido: ' . PHP_EOL;
     } else {
         echo 'Telefone invalido: ' . PHP_EOL;
     }
+
+    echo preg_replace(
+            $regex,
+            '(XX) \2',
+            $telefone
+        ) . PHP_EOL;
 }
+
