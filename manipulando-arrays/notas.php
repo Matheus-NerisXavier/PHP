53e0925208573158dbad5e25aa9d0d0a601e75e2
Matheus Neris <matheus.nerisxa@outlook.com>
2025-07-17 20:26:12 -0300

    fazendo verificacoes

--- manipulando-arrays/notas.php
+++ manipulando-arrays/notas.php
@@ -19,3 +19,20 @@ if (is_array($notas)){
 }
 
 var_dump(array_is_list($notas));
+
+echo "Matheus fez a prova:" . PHP_EOL;
+//var_dump(isset($notas["Matheus"]));
+var_dump(array_key_exists('Matheus', $notas));
+
+
+echo "Alguém tirou 10?" . PHP_EOL;
+var_dump(in_array(10, $notas, true));
+
+echo "Quem tirou 10?" . PHP_EOL;
+echo array_search(10,$notas, true);
+
+
+// array_key_exists = verifica se a chave existe
+// in_array = verifica se o valor existe
+// isset = verifica se a chave existe e não é nula
+
