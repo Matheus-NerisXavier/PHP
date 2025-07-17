8c6072f9b0ca7677d372e6c9ca040c17d20dc607
Matheus Neris <matheus.nerisxa@outlook.com>
2025-07-17 20:55:15 -0300

    Unindo arrays

---
+++ manipulando-arrays/matriculas.php
@@ -0,0 +1,21 @@
+<?php
+
+$alunos2021 = [
+    0 => "Matheus",
+    1 => "Lucas",
+    2 => "Ana",
+    3 => "Luana",
+    4 => "Paula"
+];
+
+$novosAlunos = [
+    5 => "Patricia",
+    6 => "Nico",
+    7 => "Laura",
+    8 => "Daiane"
+];
+
+//$alunos2022 = $alunos2021 + $novosAlunos;
+$alunos2022 = array_merge($alunos2021, $novosAlunos);
+
+var_dump($alunos2022);
