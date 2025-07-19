2984ddcc67d565ae6e28babb9f1e37effd446806
Matheus Neris <matheus.nerisxa@outlook.com>
2025-07-19 11:37:58 -0300

    Compact e extract

--- manipulando-arrays/tuplas.php
+++ manipulando-arrays/tuplas.php
@@ -6,6 +6,9 @@ $dados = [
     'idade' => 24
 ];
 
-['nome' => $nome, 'nota' => $nota, 'idade' => $idade] = $dados;
+//['nome' => $nome, 'nota' => $nota, 'idade' => $idade] = $dados;
 
+extract($dados);
 var_dump($nome, $nota, $idade);
+
+var_dump(compact('nome','nota', 'idade'));
