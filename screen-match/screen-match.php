0fcd0a1d0985b0c96e70fc73d822f46807e530c3
Matheus Neris <matheus.nerisxa@outlook.com>
2025-10-26 20:39:42 -0300

    Expressão condicional

--- screen-match/screen-match.php
+++ screen-match/screen-match.php
@@ -3,6 +3,9 @@
 echo "Bem-vindo(a) ao screen match!\n";
 
 $nomeFilme = "Top Gun - Maverick";
+$nomeFilme = "Thor: Ragnarok";
+$nomeFilme = "Se beber não case";
+
 $anoLancamento = $argv[1] ?? 2022;
 
 $somaDeNotas = 9;
@@ -21,9 +24,18 @@ echo "Nota do filme: $notaFilme \n";
 echo "Ano de lançamento: $anoLancamento \n";
 
 if ($anoLancamento > 2022) {
-  echo "Esse filme é um lançamento";
+  echo "Esse filme é um lançamento\n";
 } else if ($anoLancamento > 2020 && $anoLancamento <= 2022) {
-  echo "Esse filme ainda é novo";
+  echo "Esse filme ainda é novo\n";
 } else {
-  echo "Esse filme não é um lançamento";
+  echo "Esse filme não é um lançamento\n";
 }
+
+$genero = match ($nomeFilme) {
+  "Top Gun - Maverick" => "ação",
+  "Thor: Ragnarok" => "super-heroi",
+  "Se beber não case" => "comédia",
+  default => "gênero desconhecido",
+};
+
+echo "O gênero do filme é: $genero";
